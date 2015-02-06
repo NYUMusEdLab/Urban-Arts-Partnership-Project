0e7b5d0f64400116e76ff2ca96992419bf4d2e47
Spencer Tang <stang@xogrp.com>
2015-02-05 20:57:34 -0500

    Add keyword cards to show view

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,33 +1,26 @@
 $(document).ready(function(){
 
-  $('p').on("mouseenter","span",function(){
-    var data = $(this).parent().data().info
-    if (data === true){
-    $(this).css("background-color","red")
-    }
-   
+  $('body').on("click",".keyword",function(){
+    console.log($(this).data().keywordid)
+
+    $('.keyword-card').hide();
+    var keywordData = $(this).data().keywordid
+    $('.keyword-card').filter('[data-keywordid=' + keywordData + ']').fadeIn()
+    
   })
 
-  $('p').on("mouseleave","span",function(){
-    $(this).css("background-color","white")
-  })
-
-  $('p').on("click","span",function(){
-    $('.info-card').hide();
-    var data = $(this).parent().data().lyric
-    var show = $(this).parent().data().info
-    if (show === true){
-      $('section').filter('[data-definition=' + data + ']').fadeIn()
-    }
-  })
+  // $('.keyword-cards').html($('.keyword-cards').html().replace("'\'+'92'","'"))
   $('p').html($('p').html().replace("Mesopatamia","Mesopotamia"))
   var keywords = $.parseJSON($('script').last().html()).replace('["',"").replace('"]',"").split('", "')
 
   keywords.forEach(function(value,index){
-       $('p').html($('p').html().replace(value,"<strong data-keyword-id=" + index + "> "+value+" </strong>"))
-       $('p').html($('p').html().replace(value.toLowerCase(),"<span class='keyword' data-keyword-id=" + index + "> "+value.toLowerCase()+" </span>"))
+       $('p').html($('p').html().replace(value,"<span class='keyword' data-keywordid=" + index + "> "+value+" </span>"))
+       $('p').html($('p').html().replace(value.toLowerCase(),"<span class='keyword' data-keywordid=" + index + "> "+value.toLowerCase()+" </span>"))
   })
   
-
+  $('p').not(".keyword").on("click",function(){
+        $('.keyword-card').hide();
+    
+  })
 
 })
