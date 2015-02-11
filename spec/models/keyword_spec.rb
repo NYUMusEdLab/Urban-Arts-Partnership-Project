2071a4b89d148d12ffdc024f6c99514c1a106c67
Spencer Tang <stang@xogrp.com>
2015-02-10 22:01:47 -0500

    Add test for comments

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe Keyword do
   let(:keyword) {keyword.new}
+  let()
   it "can creat an instance of itself" do
     expect(keyword).to be_a_keyword
   end 
-end
+  it "has many comments" do
+    expect(keyword.comments).to eq{} 
+  end
+end 
--- spec/models/lyric_spec.rb
+++ spec/models/lyric_spec.rb
@@ -1,8 +1,8 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe Lyric do
-#   let(:lyic) {Lyric.new}
-#   it "can creat an instance of itself" do
-#     expect(lyric).to be_a_lyric
-#   end 
-# end
+describe Lyric do
+  let(:lyic) {Lyric.new}
+  it "can creat an instance of itself" do
+    expect(lyric).to be_a_lyric
+  end 
+end
