bda1eca7dc1647a813c27791a54024efabe64fa1
Spencer Tang <stang@xogrp.com>
2015-02-17 22:20:35 -0500

    Add test for keywords

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe Song do
-  let(:song) {Song.new}
+  let(:song) {Song.create(title:"geography")}
+
   it "can creat an instance of itself" do
     expect(song).to be_a_song
   end 
 
-  it "has many lyrics" do 
-    expect(song).respond_to "lyrics"
+  it "can create a keyword" do 
+    song.keywords.create(:name => "history", definition: "it's important")
+    expect(song.keywords.first.name).to eq("history") 
   end
 end
