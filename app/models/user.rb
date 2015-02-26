9668bd72eb69ab3ca7a75fe3fe496ad95dabb9b2
Spencer Tang <stang@xogrp.com>
2015-02-25 21:25:13 -0500

    Add bcrypt to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
     @password = Password.create(new_password)
     self.password_hash = @password
   end
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      give_token
+    else
+      redirect_to home_url
+    end
+  end
 end
