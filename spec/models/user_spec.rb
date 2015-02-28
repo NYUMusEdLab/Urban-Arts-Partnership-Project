b1297181ff26a6ea6bec429b18d8472a4c0974c4
Spencer Tang <stang@xogrp.com>
2015-02-27 19:56:11 -0500

    Add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+  let(:user) {User.create()}
   pending "add some examples to (or delete) #{__FILE__}"
+  it "can create a user" do 
+    user
+  end
+  it "has a password" do 
+    expect(user.password).to
+  end
 end
