74d5474cf74b2d8990a565c0e273e7f2fa584114
Spencer Tang <stang@xogrp.com>
2015-03-02 23:59:09 -0500

    Add papercli[ to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
     # users.password_hash in the database is a :string
   include BCrypt
 
+   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   def password
     @password ||= Password.new(password_hash)
   end
