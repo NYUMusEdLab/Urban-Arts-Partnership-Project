6026754fcd39b39e34777a71fe9630b2a6de9990
Spencer Tang <stang@xogrp.com>
2015-03-06 17:29:37 -0500

    Add password test

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
     @password = Password.create(new_password)
     self.password_hash = @password
   end
-  def login
+
+ def login
     @user = User.find_by_email(params[:email])
     if @user.password == params[:password]
       give_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  let(:user) {User.create()}
+  let(:user) {User.create(password: "123")}
   pending "add some examples to (or delete) #{__FILE__}"
   it "can create a user" do 
     user
   end
   it "has a password" do 
-    expect(user.password).to
+    expect(user.password).to eq("123")
   end
 end
