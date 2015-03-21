3076475afc416a468ef375e8e8a0150ac17b0d8c
Spencer Tang <stang@xogrp.com>
2015-03-20 21:27:53 -0400

    Add changes in db for bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226181436) do
+ActiveRecord::Schema.define(version: 20150321012425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20150226181436) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "password"
   end
 
 end
