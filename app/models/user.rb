ffb8e4459a3010506bd6d02f94eb7876e8bb80b5
Spencer Tang <stang@xogrp.com>
2015-03-21 11:38:03 -0400

    Add signup route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
     # users.password_hash in the database is a :string
-  include BCrypt
+  has_secure_password
 
-   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+   #has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  #validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def password
     @password ||= Password.new(password_hash)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :users
 
 
-  get '/login' => 'sessions#new'
+  get '/signup' => 'users#new'
+  get '/login' => 'session#create'
   get '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
