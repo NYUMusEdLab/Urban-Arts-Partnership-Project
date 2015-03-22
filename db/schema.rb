88f7a90f9e8e44e6612b386f8a6dbdd16d04bc61
Spencer Tang <stang@xogrp.com>
2015-03-22 16:03:21 -0400

    Add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150321012712) do
+ActiveRecord::Schema.define(version: 20150322200110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150321012712) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "email"
   end
 
 end
