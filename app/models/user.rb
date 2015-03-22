dfb9f9d38352f6d8d222e80bb3dd1801d84d0133
Spencer Tang <stang@xogrp.com>
2015-03-22 15:56:39 -0400

    Add changes to user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,20 @@ class User < ActiveRecord::Base
    #has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   #validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
+  #def password
+    #@password ||= Password.new(password_hash)
+  #end
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
- def login
-    @user = User.find_by_email(params[:email])
-    if @user.password == params[:password]
-      give_token
-    else
-      redirect_to home_url
-    end
-  end
+  #def password=(new_password)
+    #@password = Password.create(new_password)
+    #self.password_hash = @password
+  #end
+ #def login
+    #@user = User.find_by_email(params[:email])
+    #if @user.password == params[:password]
+      #give_token
+    #else
+      #redirect_to home_url
+    #end
+  #end
 end
