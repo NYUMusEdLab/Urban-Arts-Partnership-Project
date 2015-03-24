a79796bce67627479b205fabf4d6c01ad1ef139c
Spencer Tang <stang@xogrp.com>
2015-03-23 20:37:25 -0400

    Add methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,20 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.new(params[:user])
-    @user.password = params[:password]
-    @user.save!
-    redirect_to root
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
   end
-    
-  def new 
-    @user = User.new
-    redirect_to "/shows"
-    
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
